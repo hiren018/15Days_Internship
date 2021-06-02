c2d10ee0a19ccaa8ce193255843a2ee23a2fc24d
Hiren Sanghani <84366746+hiren018@users.noreply.github.com>
2021-06-02 12:49:09 +0530

    Add files via upload

---
+++ Day 6/Splash/app/src/main/java/com/hirenpatel/splash/MainActivity.java
@@ -0,0 +1,54 @@
+package com.hirenpatel.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this , R.raw.music1);
+        mediaPlayer.start();
+
+
+        Thread thread = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(10000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(MainActivity.this , MainActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.release();
+        finish();
+    }
+}
---
+++ Day 6/Splash/app/src/main/java/com/hirenpatel/splash/MainActivity2.java
@@ -0,0 +1,57 @@
+package com.hirenpatel.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main2);
+
+        mediaPlayer = MediaPlayer.create(MainActivity2.this , R.raw.music2);
+        mediaPlayer.start();
+
+
+        Thread thread = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(10000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(MainActivity2.this , MainActivity3.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.release();
+        finish();
+    }
+
+
+
+}
